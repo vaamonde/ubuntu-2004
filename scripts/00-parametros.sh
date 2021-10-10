972a85bb09ba24df0a90ca180f114dfb1e9304bc
Robson Vaamonde <robsonvaamonde@gmail.com>
2021-10-10 13:59:53 -0300

    Atualizacao

---
+++ scripts/00-parametros.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 10/10/2021
+# Data de atualização: 10/10/2021
+# Versão: 0.01
+# Testado e homologado para a versão do Ubuntu Server 20.04.x LTS x64
+# Kernel >= 4.15.x
+#
+# Parâmetros (variáveis de ambiente) utilizados nos scripts de instalação dos Serviços de Rede
+# no Ubuntu Server 20.04.x LTS, antes de modificar esse arquivo, veja os arquivos: BUGS, NEW e
+# CHANGELOG para mais informações.
+
