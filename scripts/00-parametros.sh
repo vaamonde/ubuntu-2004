6cc5a1543ddcd421886f2c3f612d9e59f5f52430
Robson Vaamonde <robsonvaamonde@gmail.com>
2021-12-09 17:58:03 -0300

    Atualizacao

--- scripts/00-parametros.sh
+++ scripts/00-parametros.sh
@@ -687,5 +687,5 @@ GUACAMOLERDEP="tomcat9 tomcat9-admin tomcat9-user bind9"
 GUACAMOLEINSTALL="libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev \
 libavcodec-dev libavformat-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev \
 libssh2-1-dev libtelnet-dev libvncserver-dev libwebsockets-dev libpulse-dev libssl-dev \
-libvorbis-dev libwebp-dev gcc-10 g++-10 make libfreeerdp2-2 freerdp2-dev freerdp2-x11"
+libvorbis-dev libwebp-dev gcc-10 g++-10 make libfreerdp2-2 freerdp2-dev freerdp2-x11"
 #
