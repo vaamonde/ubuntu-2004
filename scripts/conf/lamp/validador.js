04b3eea90156618973219b3c09c319806b1c7e30
Robson Vaamonde <robsonvaamonde@gmail.com>
2021-12-14 19:55:24 -0300

    Add files via upload

---
+++ scripts/conf/lamp/validador.js
@@ -0,0 +1,7 @@
+function validar() {
+	let username = frmFormpost.username.value
+	let password = frmFormpost.password.value
+	//document.getElementById("teste").value="oi"
+	window.location.href = "formpost.html"
+	alert(username)
+}
