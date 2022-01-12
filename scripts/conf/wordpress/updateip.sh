bf9c50fc8493a8da1514107df46cac85594140d1
Robson Vaamonde <robsonvaamonde@gmail.com>
2022-01-12 17:49:55 -0300

    Atualizacao

---
+++ scripts/conf/wordpress/updateip.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Linkedin: https://www.linkedin.com/in/robson-vaamonde-0b029028/
+# Instagram: https://www.instagram.com/procedimentoem/?hl=pt-br
+# Data de criação: 12/01/2022
+# Data de atualização: 12/01/2022
+# Versão: 0.02
+# Testado e homologado para a versão do Ubuntu Server 20.04.x LTS x64x
+# Testado e homologado para a versão do Wordpress v5.8.x
+
+# Script de atualização da alteração do Endereço IPv4 do Banco de Dados do Wordpress
+#
+# Variável para a geração do arquivo de Salt padrão do site Oficial do Wordpress
+USERNAME="wordpress"
+PASSWORD="wordpress"
+IPV4ADDRESS="10.26.0.0"
+DATABASE_WORDPRESS="wordpress"
+SELECT_WORDPRESS="SELECT * FROM wp_options WHERE option_name IN('siteurl','home');"
+UPDATE_SITEURL_WORDPRESS="UPDATE wp_options SET option_value='http://$IPV4ADDRESS' WHERE option_name = 'siteurl';"
+UPDATE_HOME_WORDPRESS="UPDATE wp_options SET option_value='http://$IPV4ADDRESS' WHERE option_name = 'home';"
+#
+echo -e "Alterando o Endereço IPv4 do Banco de Dados do Wordpress\n"
+sleep 5
+#
+echo -e "Listando o Endereço IPv4 atual do computador, aguarde..."
+	hostname -I
+echo -e "Endereço IPv4 listado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Visualizando o Endereço IPv4 configurado no Banco de Dados do Wordpress, aguarde..."
+	mysql -u $USERNAME -p$PASSWORD -e "$SELECT_WORDPRESS" $DATABASE_WORDPRESS
+echo -e "Endereço visualizado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Atualizando para o Endereço IPv4 para: $IPV4ADDRESS, aguarde..."
+	mysql -u $USERNAME -p$PASSWORD -e "$UPDATE_SITEURL_WORDPRESS" $DATABASE_WORDPRESS
+	mysql -u $USERNAME -p$PASSWORD -e "$UPDATE_HOME_WORDPRESS" $DATABASE_WORDPRESS
+echo -e "Endereço IPv4 atualizado sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Visualizando o Endereço IPv4 configurado no Banco de Dados do Wordpress, aguarde..."
+	mysql -u $USERNAME -p$PASSWORD -e "$SELECT_WORDPRESS" $DATABASE_WORDPRESS
+echo -e "Endereço visualizado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+#
+echo -e "Acesse a URL: http://$(hostname -I | cut -d' ' -f1)/wp para finalizar e testar o"
+echo -e " acesso da nova configuração do Sistema de Site Dinânicos CMS Wordpress\n"
+sleep 5
