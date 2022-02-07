fe941117efc085bc1db262107f8db2aa01ebab18
Robson Vaamonde <robsonvaamonde@gmail.com>
2022-02-07 11:29:46 -0300

    Atualizacao

--- scripts/conf/lemp/100-lemp.sh
+++ scripts/conf/lemp/100-lemp.sh
@@ -278,19 +278,4 @@ echo -e "Verificando as portas de Conexão do Nginx e do MariaDB, aguarde..."
 echo -e "Portas verificadas com sucesso!!!, continuando com o script...\n"
 sleep 5
 #
-echo -e "Instalação do LEMP-SERVER feito com Sucesso!!!"
-	# script para calcular o tempo gasto (SCRIPT MELHORADO, CORRIGIDO FALHA DE HORA:MINUTO:SEGUNDOS)
-	# opção do comando date: +%T (Time)
-	HORAFINAL=`date +%T`
-	# opção do comando date: -u (utc), -d (date), +%s (second since 1970)
-	HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
-	HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
-	# opção do comando date: -u (utc), -d (date), 0 (string command), sec (force second), +%H (hour), %M (minute), %S (second), 
-	TEMPO=`date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S"`
-	# $0 (variável de ambiente do nome do comando)
-	echo -e "Tempo gasto para execução do script $0: $TEMPO"
-echo -e "Pressione <Enter> para concluir o processo."
-# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
-echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
-read
-exit 1
+
