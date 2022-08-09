c4906a78689c6a10443954a17348e98bac474f4f
Robson Vaamonde <robsonvaamonde@gmail.com>
2022-08-09 11:54:23 -0300

    Atualizacao

--- scripts/settings/VSCode.sh
+++ scripts/settings/VSCode.sh
@@ -1,131 +1,168 @@
-#!/bin/bash
-# Autor: Robson Vaamonde
-# Procedimentos em TI: http://procedimentosemti.com.br
-# Bora para Prática: http://boraparapratica.com.br
-# Robson Vaamonde: http://vaamonde.com.br
-# Facebook Procedimentos em TI: https://www.facebook.com/ProcedimentosEmTi
-# Facebook Bora para Prática: https://www.facebook.com/BoraParaPratica
-# Instagram Procedimentos em TI: https://www.instagram.com/procedimentoem
-# YouTUBE Bora Para Prática: https://www.youtube.com/boraparapratica
-# LinkedIn Robson Vaamonde: https://www.linkedin.com/in/robson-vaamonde-0b029028/
-# Github: https://github.com/vaamonde
-# Data de criação: 23/07/2021
-# Data de atualização: 26/11/2021
-# Versão: 0.05
-# Testado e homologado no Linux Mint 20.x e VSCode 1.58.x
-#
-O Visual Studio Code é um editor de código-fonte desenvolvido pela Microsoft para Windows, 
-Linux e macOS. Ele inclui suporte para depuração, controle de versionamento Git incorporado, 
-realce de sintaxe, complementação inteligente de código, snippets e refatoração de código. 
-Ele é customizável, permitindo que os usuários possam mudar o tema do editor, teclas de atalho 
-e preferências. Ele é um software livre e de código aberto, apesar do download oficial estar 
-sob uma licença proprietária.
-#
-# Links Oficial do VSCode e do Marketplace
-Link do Visual Studio Code: https://code.visualstudio.com/
+#Autor: Robson Vaamonde<br>
+#Procedimentos em TI: http://procedimentosemti.com.br<br>
+#Bora para Prática: http://boraparapratica.com.br<br>
+#Robson Vaamonde: http://vaamonde.com.br<br>
+#Facebook Procedimentos em TI: https://www.facebook.com/ProcedimentosEmTi<br>
+#Facebook Bora para Prática: https://www.facebook.com/BoraParaPratica<br>
+#Instagram Procedimentos em TI: https://www.instagram.com/procedimentoem<br>
+#YouTUBE Bora Para Prática: https://www.youtube.com/boraparapratica<br>
+#Data de criação: 31/05/2022<br>
+#Data de atualização: 28/07/2022<br>
+#Versão: 0.02<br>
+#Testado e homologado no Linux Mint 20.1 Ulyssa, 20.2 Uma e 20.3 Una x64
+
+#Instalação do Microsoft Visual Studio Code VSCode no Linux Mint 20.1 Ulyssa, 20.2 Uma e 20.3 Una x64
+
+Site Oficial do Visual Studio Code: https://code.visualstudio.com/<br>
 Link do Marketplace: https://marketplace.visualstudio.com/VSCode
-#
-# 01_ Baixando o VSCode para o Linux Mint 20.x
-https://code.visualstudio.com/download
-	Versão: .deb (Debian, Ubuntu 64 Bits)
-		Salvar aquivo
-#
-# 02_ Instalando o Vim, Git e o Python no Linux Mint 20.x
-Terminal (Ctrl+Alt+T)
-	sudo apt update 
-	sudo apt install vim git python cloc
-	exit
-#
-# 03_ Instalando o VSCode utilizando o Gdebi-Gtk do Linux Mint 20.x
-Arquivos
-	Download
-		code_1.xxxx_amd64
-			Instalar Pacote
-		Fechar
-#
-# 04_ Verificando o novo repositório do VSCode no MintUpdate do Linux Mint 20.x
-Menu
-	MintUpdate
-		Editar
-			Fontes de Programas
-				(Digite a senha do seu usuário)
-					Repositórios Adicionais
-						Habilitado: Microsoft / Stable - code
-					Chaves de Autenticação
-						Microsoft (Release signing)
+
+#00_ Verificando as Informações do Sistema Operacional Linux Mint<br>
+
+	OBSERVAÇÃO IMPORTANTE: Linux Mint 20.3 Una é derivado do Ubuntu Desktop 20.04.4 Focal Fossa
+	sudo cat /etc/os-release
+
+#01_ Atualização do Sistema Operacional Linux Mint<br>
+
+	sudo apt update
+	sudo apt upgrade
+	sudo apt full-upgrade
+	sudo apt dist-upgrade
+	sudo apt autoremove
+	sudo apt autoclean
+
+#02_ Instalando as Dependências do Microsoft Visual Studio Code VSCode no Linux Mint<br>
+
+	sudo apt install vim git python2 python3 cloc
+
+#03_ Baixando o Microsoft Visual Studio Code VSCode para o Linux Mint<br>
+
+	https://code.visualstudio.com/download
+		Versão: .deb (Debian, Ubuntu 64 Bits)
+			Salvar aquivo
+
+#04_ Instalando o Microsoft Visual Studio Code VSCode utilizando o Gdebi-Gtk no Linux Mint<br>
+
+	Arquivos
+		Download
+			code_1.xxxx_amd64
+				Instalar Pacote
 			Fechar
-	Fechar
-#
-# 05_ Iniciando o VSCode no Linux Mint 20.x
-Menu
-	Busca Indexada
-		vscode
-			Dark Theme
-			Notifications: Pacote PT-BR
-			Disable: Mostrar página inicial na inicialização
-#
-# 06_ Configurando o VSCode como Aplicativo de Preferência no Linux Mint 20.x
-Menu
-	Busca Indexada
-		Aplicativos de Preferencias
-			Texto puro: Visual Studio Code
-			Código fonte: Visual Studio Code
-#
-# 07_ Instalando e Configurando as Principais Extensões que utilizo no Meu Dia a Dia
-Portuguese (Brazil) Language Pack for Visual Studio Code
-	(Sem necessidade de configuração)
 
-Brazilian Portuguese - Code Spell Checker (Corretor Ortográfico de Código)
-Manter selecionado a extensão: Brazilian Portuguese - Code Spell Checker
-	Pressionar F1
-		Show Spell Checker Configuration Info
-			User
-				Language
-					English (en_us)
-					Portuguese (pt_br)
-					Portuguese - Brazil (pt-br)
-				File Types and Programming Languages
-					shellscript, python, markdown, etc...
+#05_ Verificando o novo repositório do Microsoft Visual Studio Code VSCode no MintUpdate<br>
 
-Bats (Bash Automated Testing System)
-	(Sem necessidade de configuração)
+	Menu
+		MintUpdate
+			Editar
+				Fontes de Programas
+					(Digite a senha do seu usuário)
+						Repositórios Adicionais
+							Habilitado: Microsoft / Stable - code
+						Chaves de Autenticação
+							Microsoft (Release signing)
+				Fechar
+		Fechar
 
-Bash Beautify
-	(Sem necessidade de configuração)
+#06_ Iniciando o Microsoft Visual Studio Code VSCode no Linux Mint<br>
 
-Shell-Format
-	(Sem necessidade de configuração)
+	Menu
+		Busca Indexada
+			vscode
+				Dark Theme
+				Notifications: Pacote PT-BR
+				Disable: Mostrar página inicial na inicialização
 
-ShellCheck
-	(Sem necessidade de configuração)
+#07_ Configurando o Microsoft Visual Studio Code VSCode como Aplicativo de Preferência no Linux Mint<br>
 
-Cisco IOS Systax
-	(Sem necessidade de configuração)
+	Menu
+		Busca Indexada
+			Aplicativos de Preferencias
+				Texto puro: Visual Studio Code
+				Código fonte: Visual Studio Code
 
-Cisco IOS-XR Systax
-	(Sem necessidade de configuração)
+#08_ Instalando e Configurando as Principais Extensões que utilizo no Meu Dia a Dia<br>
 
-Cisco Config Highlight
-	(Sem necessidade de configuração)
+	Portuguese (Brazil) Language Pack for Visual Studio Code
+		(Sem necessidade de configuração)
 
-Pylance
-	(Sem necessidade de configuração)
+	Brazilian Portuguese - Code Spell Checker (Corretor Ortográfico de Código)
+	Manter selecionado a extensão: Brazilian Portuguese - Code Spell Checker
+		Pressionar F1
+			Show Spell Checker Configuration Info
+				User
+					Language
+						English (en_us)
+						Portuguese (pt_br)
+						Portuguese - Brazil (pt-br)
+					File Types and Programming Languages
+						shellscript, python, markdown, etc...
 
-Python
-	(Sem necessidade de configuração)
-#
-# 08_ Configurações principais do VSCode para funcionar perfeitamente no Linux Mint
-Gerenciar
-	Configurações
-		Code Spell Checker
-			C Spell: Enabled Language Ids: 
-				Adicionar Item: shellscript
-			C Spell: Language: en,pt,pt-BR
-			C Spell: Max Duplicate Problems: 500000
-			C Spell: Max Number Of Problems: 500000
-		Editor
-			Editor: Tab Size: 4
-			Editor: Detect Indentation: False (Off)
-			Editor: Insert Spaces: False (Off)
-		Files
-			Files: Eol: \n (LF)
+	Code Spell Checker
+		(Sem necessidade de configuração)
+
+	Bats (Bash Automated Testing System)
+		(Sem necessidade de configuração)
+
+	Bash Beautify
+		(Sem necessidade de configuração)
+
+	Shell-Format
+		(Sem necessidade de configuração)
+
+	ShellCheck
+		(Sem necessidade de configuração)
+
+	Cisco IOS Systax
+		(Sem necessidade de configuração)
+
+	Cisco IOS-XR Systax
+		(Sem necessidade de configuração)
+
+	Cisco Config Highlight
+		(Sem necessidade de configuração)
+
+	Pylance
+		(Sem necessidade de configuração)
+
+	Python
+		(Sem necessidade de configuração)
+
+	Docker
+		(Sem necessidade de configuração)
+
+	Powershell
+		(Sem necessidade de configuração)
+
+	Ansible
+		(Sem necessidade de configuração)
+
+	YAML
+		(Sem necessidade de configuração)
+
+#09_ Configurações principais do Microsoft Visual Studio Code VSCode para funcionar perfeitamente no Linux Mint<br>
+
+	Gerenciar
+		Configurações
+			Code Spell Checker
+				C Spell: Enabled Language Ids: 
+					Adicionar Item: shellscript
+				C Spell: Language: en,pt,pt-BR
+				C Spell: Max Duplicate Problems: 500000
+				C Spell: Max Number Of Problems: 500000
+			Editor
+				Editor: Tab Size: 4
+				Editor: Detect Indentation: False (Off)
+				Editor: Insert Spaces: False (Off)
+				Render Whitespace: All
+			Files
+				Files: Eol: \n (LF)
+
+			#OBSERVAÇÃO IMPORTANTE: executar essa configuração somente se você fez a instalação
+			#do ZSH, das Fontes Hack e do Oh My ZSH.
+			Font
+				Integrated: Font Family
+					Hack Nerd Font
+			
+			#Configuração do Terminal Padrão do VSCODE
+			Ctrl + Shift + P
+				Terminal: Selecionar o Perfil Padrão
+					zsh
