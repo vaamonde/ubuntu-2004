0dbb3cf9d7e3e7dfd054aea639c1add17fbecf09
Robson Vaamonde <robsonvaamonde@gmail.com>
2022-08-21 19:01:23 -0300

    Atualizacao

---
+++ scripts/21-grafana.sh
@@ -0,0 +1,265 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Linkedin: https://www.linkedin.com/in/robson-vaamonde-0b029028/
+# Instagram: https://www.instagram.com/procedimentoem/?hl=pt-br
+# Github: https://github.com/vaamonde
+# Data de criação: 11/12/2021
+# Data de atualização: 20/08/2022
+# Versão: 0.06
+# Testado e homologado para a versão do Ubuntu Server 20.04.x LTS x64x
+# Testado e homologado para a versão do Grafana Server v9.1.x
+#
+# Grafana é uma aplicação web de análise de código aberto multiplataforma e visualização 
+# interativa da web. Ele fornece tabelas, gráficos e alertas para a Web quando conectado 
+# a fontes de dados suportadas. É expansível através de um sistema de plug-in.
+#
+# Informações que serão solicitadas na configuração via Web do Grafana
+# Email or username: admin
+# Password: admin: (Log In)
+# Change Password
+#	New password: pti@2018
+#	Confirm new password: pti@2018: (Submit)
+#
+# Site Oficial do Projeto Grafana: https://grafana.com/
+#
+# Arquivo de configuração dos parâmetros utilizados nesse script
+source 00-parametros.sh
+#
+# Configuração da variável de Log utilizado nesse script
+LOG=$LOGSCRIPT
+#
+# Verificando se o usuário é Root e se a Distribuição é >= 20.04.x 
+# [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria 
+# dos erros comuns na execução
+clear
+if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "20.04" ]
+	then
+		echo -e "O usuário é Root, continuando com o script..."
+		echo -e "Distribuição é >= 20.04.x, continuando com o script..."
+		sleep 5
+	else
+		echo -e "Usuário não é Root ($USUARIO) ou a Distribuição não é >= 20.04.x ($UBUNTU)"
+		echo -e "Caso você não tenha executado o script com o comando: sudo -i"
+		echo -e "Execute novamente o script para verificar o ambiente."
+		exit 1
+fi
+#
+# Verificando o acesso a Internet do servidor Ubuntu Server
+# [ ] = teste de expressão, exit 1 = A maioria dos erros comuns na execução
+# $? código de retorno do último comando executado, ; execução de comando, 
+# opção do comando nc: -z (scan for listening daemons), -w (timeouts), 1 (one timeout), 443 (port)
+if [ "$(nc -zw1 google.com 443 &> /dev/null ; echo $?)" == "0" ]
+	then
+		echo -e "Você tem acesso a Internet, continuando com o script..."
+		sleep 5
+	else
+		echo -e "Você NÃO tem acesso a Internet, verifique suas configurações de rede IPV4"
+		echo -e "e execute novamente este script."
+		sleep 5
+		exit 1
+fi
+#
+# Verificando se a porta 3000 está sendo utilizada no servidor Ubuntu Server
+# [ ] = teste de expressão, == comparação de string, exit 1 = A maioria dos erros comuns na execução,
+# $? código de retorno do último comando executado, ; execução de comando, 
+# opção do comando nc: -v (verbose), -z (DCCP mode), &> redirecionador de saída de erro
+if [ "$(nc -vz 127.0.0.1 $PORTGRAFANA &> /dev/null ; echo $?)" == "0" ]
+	then
+		echo -e "A porta: $PORTGRAFANA já está sendo utilizada nesse servidor."
+		echo -e "Verifique o serviço associado a essa porta e execute novamente esse script.\n"
+		sleep 5
+		exit 1
+	else
+		echo -e "A porta: $PORTGRAFANA está disponível, continuando com o script..."
+		sleep 5
+fi
+#
+# Verificando se as dependências do Grafana Server estão instaladas
+# opção do dpkg: -s (status), opção do echo: -e (interpretador de escapes de barra invertida), 
+# -n (permite nova linha), || (operador lógico OU), 2> (redirecionar de saída de erro STDERR), 
+# && = operador lógico AND, { } = agrupa comandos em blocos, [ ] = testa uma expressão, retornando 
+# 0 ou 1, -ne = é diferente (NotEqual)
+echo -n "Verificando as dependências do Grafana Server, aguarde... "
+	for name in $GRAFANADEP
+	do
+  		[[ $(dpkg -s $name 2> /dev/null) ]] || { 
+              echo -en "\n\nO software: $name precisa ser instalado. \nUse o comando 'apt install $name'\n";
+              deps=1; 
+              }
+	done
+		[[ $deps -ne 1 ]] && echo "Dependências.: OK" || { 
+            echo -en "\nInstale as dependências acima e execute novamente este script\n";
+            echo -en "Recomendo utilizar o script: 03-dns.sh para resolver as dependências."
+			echo -en "Recomendo utilizar o script: 07-lamp.sh para resolver as dependências."
+            exit 1; 
+            }
+		sleep 5
+#
+# Verificando se o script já foi executado mais de 1 (uma) vez nesse servidor
+# OBSERVAÇÃO IMPORTANTE: OS SCRIPTS FORAM PROJETADOS PARA SEREM EXECUTADOS APENAS 1 (UMA) VEZ
+if [ -f $LOG ]
+	then
+		echo -e "Script $0 já foi executado 1 (uma) vez nesse servidor..."
+		echo -e "É recomendado analisar o arquivo de $LOG para informações de falhas ou erros"
+		echo -e "na instalação e configuração do serviço de rede utilizando esse script..."
+		echo -e "Todos os scripts foram projetados para serem executados apenas 1 (uma) vez."
+		sleep 5
+		exit 1
+	else
+		echo -e "Primeira vez que você está executando esse script, tudo OK, agora só aguardar..."
+		sleep 5
+fi
+#
+# Script de instalação do Grafana Server no GNU/Linux Ubuntu Server 20.04.x
+# opção do comando echo: -e (enable interpretation of backslash escapes), \n (new line)
+# opção do comando hostname: -d (domain)
+# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
+# opção do comando cut: -d (delimiter), -f (fields)
+echo -e "Início do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n" &>> $LOG
+clear
+echo
+#
+echo -e "Instalação do Grafana Server no GNU/Linux Ubuntu Server 20.04.x\n"
+echo -e "Porta padrão utilizada pelo Grafana Server.: TCP 3000\n"
+echo -e "Após a instalação do Grafana Server acessar a URL: http://$(hostname -d | cut -d' ' -f1):3000\n"
+echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet...\n"
+sleep 5
+#
+echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
+	# Universe - Software de código aberto mantido pela comunidade:
+	# opção do comando: &>> (redirecionar a saída padrão)
+	add-apt-repository universe &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Adicionando o Repositório Multiversão do Apt, aguarde..."
+	# Multiverse – Software não suportado, de código fechado e com patente: 
+	# opção do comando: &>> (redirecionar a saída padrão)
+	add-apt-repository multiverse &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Adicionando o Repositório Restrito do Apt, aguarde..."
+	# Restricted - Software de código fechado oficialmente suportado:
+	# opção do comando: &>> (redirecionar a saída padrão)
+	add-apt-repository restricted &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Atualizando as listas do Apt, aguarde..."
+	#opção do comando: &>> (redirecionar a saída padrão)
+	apt update &>> $LOG
+echo -e "Listas atualizadas com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Atualizando todo o sistema operacional, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	# opção do comando apt: -y (yes)
+	apt -y upgrade &>> $LOG
+	apt -y dist-upgrade &>> $LOG
+	apt -y full-upgrade &>> $LOG
+echo -e "Sistema atualizado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Removendo todos os software desnecessários, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	# opção do comando apt: -y (yes)
+	apt -y autoremove &>> $LOG
+echo -e "Software removidos com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Iniciando a Instalação e Configuração do Grafana Server, aguarde...\n"
+sleep 5
+#
+echo -e "Instalando o Repositório do Grafana Server, aguarde..."
+	# opção do comando: &>> (redirecionar de saída padrão)
+	# opção do comando: | piper (conecta a saída padrão com a entrada padrão de outro comando)
+	# opção do comando wget: -q (quiet) -O (output document file)
+	wget -q -O - $GRAFANAGPGKEY | apt-key add - &>> $LOG
+	add-apt-repository "$GRAFANAAPT" &>> $LOG
+echo -e "Repositório instalado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Atualizando as listas do Apt com o novo Repositório do Grafana Server, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	apt update &>> $LOG
+echo -e "Listas atualizadas com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Instalando o Grafana Server, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	# opção do comando apt: -y (yes)
+	apt -y install grafana &>> $LOG
+echo -e "Grafana instalado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Atualizandos os arquivos de configuração Grafana Server, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	# opção do comando mv: -v (verbose)
+	# opção do comando cp: -v (verbose)
+	# opção do comando chmod: -v (verbose), 640 (User: RW-, Group: R--, Other: ---)
+	# opção do comando chown: -v (verbose), :grafana (group)
+	mv -v /etc/default/grafana-server /etc/default/grafana-server.old &>> $LOG
+	mv -v /etc/grafana/grafana.ini /etc/grafana/grafana.ini.old &>> $LOG
+	cp -v conf/grafana/grafana-server /etc/default/ &>> $LOG
+	cp -v conf/grafana/grafana.ini /etc/grafana/ &>> $LOG
+	chmod 640 -v /etc/grafana/grafana.ini &>> $LOG
+	chown :grafana /etc/grafana/grafana.ini &>> $LOG
+echo -e "Arquivos atualizados com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Editando o arquivo de configuração grafana-server, pressione <Enter> para continuar..."
+	# opção do comando read: -s (Do not echo keystrokes)
+	read -s
+	vim /etc/default/grafana-server
+echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Editando o arquivo de configuração grafana.ini, pressione <Enter> para continuar..."
+	# opção do comando read: -s (Do not echo keystrokes)
+	read -s
+	vim /etc/grafana/grafana.ini
+echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Iniciando o serviço do Grafana Server, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	systemctl enable grafana-server &>> $LOG
+	systemctl restart grafana-server &>> $LOG
+echo -e "Serviço iniciado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Verificando o serviço do Grafana Server, aguarde..."
+	echo -e "Grafana: $(systemctl status grafana-server | grep Active)"
+echo -e "Serviço verificado com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Verificando a porta de conexão do Grafana Server, aguarde..."
+	# opção do comando lsof: -n (inhibits the conversion of network numbers to host names for 
+	# network files), -P (inhibits the conversion of port numbers to port names for network files), 
+	# -i (selects the listing of files any of whose Internet address matches the address specified 
+	# in i), -s (alone directs lsof to display file size at all times)
+	lsof -nP -iTCP:3000 -sTCP:LISTEN
+echo -e "Porta verificada com sucesso!!!, continuando com o script...\n"
+sleep 5
+#
+echo -e "Instalação do Grafana Server feita com Sucesso!!!."
+	# script para calcular o tempo gasto (SCRIPT MELHORADO, CORRIGIDO FALHA DE HORA:MINUTO:SEGUNDOS)
+	# opção do comando date: +%T (Time)
+	HORAFINAL=$(date +%T)
+	# opção do comando date: -u (utc), -d (date), +%s (second since 1970)
+	HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
+	HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
+	# opção do comando date: -u (utc), -d (date), 0 (string command), sec (force second), +%H (hour), %M (minute), %S (second), 
+	TEMPO=$(date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S")
+	# $0 (variável de ambiente do nome do comando)
+	echo -e "Tempo gasto para execução do script $0: $TEMPO"
+echo -e "Pressione <Enter> para concluir o processo."
+# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
+echo -e "Fim do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n" &>> $LOG
+read
+exit 1
