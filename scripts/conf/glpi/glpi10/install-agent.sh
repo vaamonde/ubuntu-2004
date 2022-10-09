6cbd137eb1d1782b2ece9dece8894bd18b14425f
Robson Vaamonde <robsonvaamonde@gmail.com>
2022-10-09 15:00:37 -0300

    Atualizacao

---
+++ scripts/conf/glpi/glpi10/install-agent.sh
@@ -0,0 +1,47 @@
+GLPI Help Desk v10 Agent - GNU/Linux Dependęncias do Perl
+
+File::Which 				libfile-which-perl
+LWP::UserAgent				liblwp-useragent-determined-perl
+Net::IP						libnet-ip-perl
+Text::Template				libtext-template-perl
+UNIVERSAL::require			libuniversal-require-perl
+XML::TreePP					libxml-treepp-perl
+Cpanel::JSON::XS			libcpanel-json-xs-perl
+Compress::Zlib				libcompress-raw-zlib-perl libio-compress-perl
+HTTP::Daemon				libhttp-daemon-perl
+IO::Socket::SSL				libio-socket-ssl-perl
+LWP::Protocol::https		liblwp-protocol-https-perl
+Proc::Daemon				libproc-daemon-perl
+Proc::PID::File				libproc-pid-file-perl
+Net::CUPS					libnet-cups-perl
+Parse::EDID					libparse-edid-perl
+DateTime					libdatetime-perl
+Thread::Queue				libthread-queue-any-perl
+Net::NBName					libnet-nbname-perl
+Net::SNMP					libnet-snmp-perl
+Crypt::DES					libcrypt-des-perl
+Net::Write::Layer2			libnet-write-perl
+Archive::Extract			libarchive-extract-perl
+Digest::SHA					libdigest-sha-perl
+File::Copy::Recursive		libfile-copy-recursive-perl
+JSON::PP					libjson-pp-perl
+URI::Escape					liburi-escape-xs-perl
+Net::Ping					libnet-ping-external-perl
+Parallel::ForkManager		libparallel-forkmanager-perl
+Perl::Dist::Strawberry		***
+
+apt update
+
+apt install -y dmidecode lspci hdparm monitor-get-edid-using-vbe \
+monitor-get-edid get-edid ssh-keyscan arp 7zip
+
+apt install -y libfile-which-perl liblwp-useragent-determined-perl \
+libnet-ip-perl libtext-template-perl libuniversal-require-perl \
+libxml-treepp-perl libcpanel-json-xs-perl libcompress-raw-zlib-perl \
+libio-compress-perl libhttp-daemon-perl libio-socket-ssl-perl \
+liblwp-protocol-https-perl libproc-daemon-perl libproc-pid-file-perl \
+libnet-cups-perl libparse-edid-perl libthread-queue-any-perl \
+libdatetime-perl libnet-nbname-perl libnet-snmp-perl libcrypt-des-perl \
+libnet-write-perl libarchive-extract-perl libfile-copy-recursive-perl \
+libdigest-sha-perl libjson-pp-perl libnet-ping-external-perl \
+liburi-escape-xs-perl libparallel-forkmanager-perl
